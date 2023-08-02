dc1efdf0b6efd3d9f90acb125b1b30538c6e52ce
Phillip Conrad <pconrad@cs.ucsb.edu>
2023-08-02 14:28:48 -0700

    pc - fix tests that were failing on CI because axios mock wasn't in place

--- frontend/src/tests/pages/Placeholder/PlaceholderCreatePage.test.js
+++ frontend/src/tests/pages/Placeholder/PlaceholderCreatePage.test.js
@@ -3,14 +3,28 @@ import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage"
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+
 describe("PlaceholderCreatePage tests", () => {
+
+    const axiosMock = new AxiosMockAdapter(axios);
+
+    const setupUserOnly = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    };
 
     const queryClient = new QueryClient();
     test("Renders expected content", () => {
         // arrange
 
-        const expectedText = "Create page not yet implemented";
-
+        setupUserOnly();
+       
         // act
         render(
             <QueryClientProvider client={queryClient}>
@@ -21,7 +35,7 @@ describe("PlaceholderCreatePage tests", () => {
         );
 
         // assert
-        expect(screen.getByText(expectedText)).toBeInTheDocument();
+        expect(screen.getByText("Create page not yet implemented")).toBeInTheDocument();
     });
 
 });
--- frontend/src/tests/pages/Placeholder/PlaceholderIndexPage.test.js
+++ frontend/src/tests/pages/Placeholder/PlaceholderIndexPage.test.js
@@ -3,13 +3,28 @@ import PlaceholderIndexPage from "main/pages/Placeholder/PlaceholderIndexPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+
+
 describe("PlaceholderIndexPage tests", () => {
+
+    const axiosMock = new AxiosMockAdapter(axios);
+
+    const setupUserOnly = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    };
 
     const queryClient = new QueryClient();
     test("Renders expected content", () => {
         // arrange
 
-        const expectedText = "Index page not yet implemented";
+        setupUserOnly();
 
         // act
         render(
@@ -21,7 +36,7 @@ describe("PlaceholderIndexPage tests", () => {
         );
 
         // assert
-        expect(screen.getByText(expectedText)).toBeInTheDocument();
+        expect(screen.getByText("Index page not yet implemented")).toBeInTheDocument();
         expect(screen.getByText("Create")).toBeInTheDocument();
         expect(screen.getByText("Edit")).toBeInTheDocument();
     });
