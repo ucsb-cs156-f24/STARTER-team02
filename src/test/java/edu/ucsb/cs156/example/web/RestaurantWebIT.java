ae183018abfcee31edc869261eecf5b80365fdf5
Andrew Peng <andrew.z.peng@gmail.com>
2024-05-19 14:49:51 -0700

    ap - more workflow testing

--- src/test/java/edu/ucsb/cs156/example/web/RestaurantWebIT.java
+++ src/test/java/edu/ucsb/cs156/example/web/RestaurantWebIT.java
@@ -34,7 +34,7 @@ public class RestaurantWebIT extends WebTestCase {
 
         page.getByTestId("RestaurantTable-cell-row-0-col-Edit-button").click();
         assertThat(page.getByText("Edit Restaurant")).isVisible();
-        page.getByTestId("RestaurantForm-description").fill("THE BESTEST");
+        page.getByTestId("RestaurantForm-description").fill("THE BEST");
         page.getByTestId("RestaurantForm-submit").click();
 
         assertThat(page.getByTestId("RestaurantTable-cell-row-0-col-description")).hasText("THE BESTEST");
