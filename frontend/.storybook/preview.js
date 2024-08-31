9edc863e345244d42cb7291a41b5e50438e72cbf
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-08-31 12:43:00 -0700

    pc - when running on github pages, get mockServiceWorker.js from organization repo for github pages

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -10,8 +10,20 @@ import { ToastContainer } from "react-toastify";
 
 const queryClient = new QueryClient();
 
+const currentUrl = window.location.href;
+const isLocalhost = currentUrl.startsWith("http://localhost:6006/");
+const mockServiceWorkerUrl = isLocalhost ? "mockServiceWorker.js" : "https://" + window.location.hostname + "/mockServiceWorker.js";
+
 // Initialize MSW
-initialize()
+
+initialize(
+  {
+    serviceWorker: {
+      url: mockServiceWorkerUrl
+    }
+  }
+);
+
 
 // Per https://storybook.js.org/docs/react/writing-stories/decorators#context-for-mocking
 // Here, we provide the context needed for some of the components,
